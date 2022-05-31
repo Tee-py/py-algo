1100b87655db27c41a5c12fe0b21b488aa3e501e
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-05-31 14:20:52 +0100

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,5 +1,5 @@
 import re
-
+"""
 class Dog:
 
     def __init__(self, name, sex):
@@ -16,55 +16,6 @@ class Dog:
     
     def __repr__(self):
         return self.name
-
-class CreditCard:
-
-    def __init__(self, customer, bank, card_no, limit):
-        self._customer = customer
-        self._bank = bank
-        self._card_no = card_no
-        self._balance = 0
-        self._limit = limit
-
-    def __repr__(self):
-        return f"{self._customer} - {self._card_no}"
-    #Overloading the addition operator for the class
-    def __add__(self, cl):
-        return self._balance + cl._balance
-
-    #Overloading the boolean operator for the class Instances
-    def __bool__(self):
-        return True if self.get_balance()>0 else False
-
-
-    def get_customer(self):
-        return self._customer
-
-    def get_balance(self):
-        return self._balance
-
-    def get_bank(self):
-        return self._bank
-
-    def get_card_no(self):
-        return self._card_no
-
-    def get_limit(self):
-        return self._limit
-
-    def credit(self, amount):
-        if amount + self._balance > self._limit:
-            print("Limit Exceeded")
-        else:
-            self._balance += amount
-            print("Credit Successfull")
-
-    def debit(self, amount):
-        if amount > self.get_balance():
-            print("Insufficient Funds")
-        else:
-            self._balance -= amount
-            print(f"a sum of {amount} was debited successfully")
 
 class Vector:
 
@@ -101,7 +52,6 @@ class Vector:
     def __str__(self):
         return f"<{str(self._coords)[1:-1]}>"
 
-
 class Polynomial:
 
     def __init__(self, expr):
@@ -111,11 +61,80 @@ class Polynomial:
     def extract_members(self):
         member_regex = re.compile("")
 
-
-
 class Sequence:
 
     def __init__(self, seq_type):
         self.type = seq_type
         print("Initiated sequence object")
+"""
+
+class CreditCard:
+
+    def __init__(self, customer, bank, card_no, limit):
+        self._customer = customer
+        self._bank = bank
+        self._card_no = card_no
+        self._balance = 0
+        self._limit = limit
+
+    def __repr__(self):
+        return f"{self._customer} - {self._card_no}"
+
+    #Overloading the addition operator for the class
+    def __add__(self, cl):
+        return self._balance + cl._balance
+
+    #Overloading the boolean operator for the class Instances
+    def __bool__(self):
+        return True if self.balance > 0 else False
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, name):
+        self._customer = name
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def bank(self):
+        return self._bank
+    
+    @bank.setter
+    def bank(self, new_bank):
+        self.bank = new_bank
+
+    @property
+    def card_no(self):
+        return self._card_no
+
+    @property
+    def limit(self):
+        return self._limit
+
+    def credit(self, amount):
+        if amount + self._balance > self._limit:
+            print("Limit Exceeded")
+        else:
+            self._balance += amount
+            print("Credit Successfull")
+
+    def debit(self, amount):
+        if amount > self.get_balance():
+            print("Insufficient Funds")
+        else:
+            self._balance -= amount
+            print(f"a sum of {amount} was debited successfully")
+    
+    @staticmethod
+    def test():
+        print("Yayyyy")
+
+
+
+
 
